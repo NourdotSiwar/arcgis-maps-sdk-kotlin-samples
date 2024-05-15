0715d395a61dbdac903bb301fe963d66edc83c22
Shubham Sharma <shubhamsharma@esri.com>
2024-05-15 11:04:41 -0700

    auth tutorial

--- show-portal-user-info/src/main/java/com/esri/arcgismaps/sample/showportaluserinfo/screens/MainScreen.kt
+++ show-portal-user-info/src/main/java/com/esri/arcgismaps/sample/showportaluserinfo/screens/MainScreen.kt
@@ -17,49 +17,15 @@
 package com.esri.arcgismaps.sample.showportaluserinfo.screens
 
 import android.app.Application
-import android.graphics.Bitmap
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Alignment.Companion.Center
-import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.arcgismaps.toolkit.authentication.AuthenticatorState
 import com.arcgismaps.toolkit.authentication.DialogAuthenticator
-import com.esri.arcgismaps.sample.sampleslib.components.MessageDialog
-import com.esri.arcgismaps.sample.sampleslib.components.SampleTopAppBar
 import com.esri.arcgismaps.sample.showportaluserinfo.components.AppViewModel
 
 /**
@@ -71,184 +37,11 @@ fun MainScreen(sampleName: String, application: Application) {
     val appViewModel = viewModel { AppViewModel(application) }
     val authenticatorState: AuthenticatorState = appViewModel.authenticatorState
 
-    Scaffold(
-        topBar = { SampleTopAppBar(title = sampleName) },
-        content = {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(it)
-            ) {
-                val infoText = appViewModel.infoText.collectAsState().value
-                val isLoading = appViewModel.isLoading.collectAsState().value
-                PortalDetails(
-                    url = appViewModel.url.collectAsState().value,
-                    onSetUrl = appViewModel::setUrl,
-                    onSignOut = appViewModel::signOut,
-                    onLoadPortal = appViewModel::loadPortal
-                )
-                InfoScreen(
-                    infoText = infoText,
-                    username = appViewModel.portalUserName.collectAsState().value,
-                    email = appViewModel.emailID.collectAsState().value,
-                    creationDate = appViewModel.userCreationDate.collectAsState().value,
-                    portalName = appViewModel.portalName.collectAsState().value,
-                    userThumbnail = appViewModel.userThumbnail.collectAsState().value,
-                    isLoading = isLoading
-                )
-                // display a dialog if the sample encounters an error
-                appViewModel.messageDialogVM.apply {
-                    if (dialogStatus) {
-                        MessageDialog(
-                            title = messageTitle,
-                            description = messageDescription,
-                            onDismissRequest = ::dismissDialog
-                        )
-                    }
-                }
-            }
-            DialogAuthenticator(authenticatorState = authenticatorState)
-        }
-    )
-}
-
-/**
- * Allows the user to enter a [url] and load a portal.
- * It uses OAuth under the hood, and has a button to clear credentials.
- *
- */
-@Composable
-private fun PortalDetails(
-    url: String,
-    onSetUrl: (String) -> Unit,
-    onSignOut: () -> Unit,
-    onLoadPortal: () -> Unit
-) {
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-        horizontalAlignment = CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        // The Url text field
-        OutlinedTextField(
-            modifier = Modifier.fillMaxWidth(),
-            value = url,
-            onValueChange = onSetUrl,
-            label = { Text("Portal URL") },
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Uri,
-                imeAction = ImeAction.Go
-            ),
-            keyboardActions = KeyboardActions(onAny = { onLoadPortal() }),
-            singleLine = true
-        )
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            val keyboardController = LocalSoftwareKeyboardController.current
-            val focusManager = LocalFocusManager.current
-            // Clear credential button
-            Button(
-                onClick = {
-                    onSignOut()
-                    keyboardController?.hide()
-                    focusManager.clearFocus()
-                },
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiary)
-            ) {
-                Text(text = "Sign out")
-            }
-            // Load button
-            Button(
-                onClick = {
-                    onLoadPortal()
-                    keyboardController?.hide()
-                    focusManager.clearFocus()
-                },
-                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
-            ) {
-                Text(text = "Load portal")
-            }
-        }
+    Surface(modifier = Modifier.fillMaxSize()) {
+        // sign out if previous login is cached
+        appViewModel.signOut()
+        // load the secured portal
+        appViewModel.loadPortal()
+        DialogAuthenticator(authenticatorState = authenticatorState)
     }
 }
-
-/**
- * Displays messages to the user. This may be used to display instructions, portal info, or error messages.
- *
- */
-@Composable
-private fun InfoScreen(
-    infoText: String,
-    username: String,
-    email: String,
-    creationDate: String,
-    portalName: String,
-    userThumbnail: Bitmap,
-    isLoading: Boolean
-) {
-    Box(
-        Modifier
-            .fillMaxSize()
-            .padding(8.dp),
-    ) {
-        LazyColumn {
-            item {
-                Box(Modifier.fillMaxWidth()) {
-                    if (isLoading) CircularProgressIndicator(
-                        Modifier.align(Center).padding(10.dp)
-                    )
-                    else Text(
-                        modifier = Modifier.align(Center).padding(10.dp),
-                        textAlign = TextAlign.Center,
-                        text = infoText
-                    )
-                }
-                HorizontalDivider()
-                Row(
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Image(
-                        bitmap = userThumbnail.asImageBitmap(),
-                        contentDescription = "User Thumbnail",
-                        contentScale = ContentScale.Crop,
-                        modifier = Modifier
-                            .clip(CircleShape)
-                            .size(150.dp)
-                    )
-                }
-                HorizontalDivider()
-                Row(modifier = Modifier.padding(10.dp)) {
-                    Text(text = "Username: ", fontWeight = FontWeight.Bold)
-                    Text(text = username)
-                }
-                HorizontalDivider()
-                Row(modifier = Modifier.padding(10.dp)) {
-                    Text(text = "E-mail: ", fontWeight = FontWeight.Bold)
-                    Text(text = email)
-                }
-                HorizontalDivider()
-                Row(modifier = Modifier.padding(10.dp)) {
-                    Text(text = "Member Since: ", fontWeight = FontWeight.Bold)
-                    Text(text = creationDate)
-                }
-                HorizontalDivider()
-
-                Row(modifier = Modifier.padding(10.dp)) {
-                    Text(text = "Portal Name: ", fontWeight = FontWeight.Bold)
-                    Text(text = portalName)
-                }
-                HorizontalDivider()
-            }
-        }
-    }
-}
-
-
